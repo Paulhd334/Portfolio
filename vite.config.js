0d2eee29a04b6bb1b035bfe86c2bfa4463096d2c
Paulhd334 <vancaemerbekepaul@gmail.com>
2025-03-09 17:12:21 +0100

    Update base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Change "/Portfolio/" en "./" pour utiliser un chemin relatif
+  base: '/Portfolio/', // Utilise le sous-dossier pour GitHub Pages (ajuste si nécessaire)
 })
