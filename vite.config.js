0367685d88645afad955845d5dd3fa7003576a0d
Paulhd334 <vancaemerbekepaul@gmail.com>
2025-03-09 17:18:11 +0100

    Fix asset paths and update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // Utilise le sous-dossier pour GitHub Pages (ajuste si nécessaire)
+  base: '/Portfolio/',  // Indique que ton site sera hébergé dans le sous-dossier 'Portfolio'
 })
